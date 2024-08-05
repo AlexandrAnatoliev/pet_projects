0bed496372275c9e16be4a338ea81620fa98488a
Александр Анатольев <per-1986@list.ru>
2024-08-05 22:32:42 +0300

    тестовая статическая библиотека

---
+++ test_lib/main.c
@@ -0,0 +1,9 @@
+// main.c - тест работы библиотеки
+
+#include "../timer_lib/timer_lib.h"
+
+int main(void)
+{
+	hello();
+	return 0;
+}
---
+++ timer_lib/source/timer_lib.c
@@ -0,0 +1,9 @@
+// timer_lib.c - статическая библиотека для измерения времени работы программы - исходный код
+
+#include <stdio.h>
+
+void hello(void)
+{
+	printf("Hello world\n");
+}
+
