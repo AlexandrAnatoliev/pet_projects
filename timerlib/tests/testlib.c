adc95df1d7826ded4e631ae9210dd6d73fff7db7
Александр Анатольев <per-1986@list.ru>
2024-08-08 21:56:18 +0300

    v1.3.0 дописал тесты

--- timerlib/tests/testlib.c
+++ timerlib/tests/testlib.c
@@ -1,19 +1,31 @@
 // testlib.c - тесты для проверки библиотеки timerlib.h
 
+#include <stdio.h>
 #include "../timerlib.h"
 
 int main(void)
 {
-	union time_u time;
+	int cnt = 6;	// итераций
+	int limit = 1;	// предел перебора значений в цикле
 
-	start(&time);
-	for(int i = 1; i < 10000; i++)
-	{
-		for(int j = 1; j < 1000; j++)
-			i/j;
+	while(cnt)
+	{	
+		union time_u time;
+
+		start(&time);
+		for(int i = 1; i < limit; i++)
+		{
+			for(int j = 1; j < limit; j++)
+				i/j;
+		}
+
+		stop(&time);
+		printf("Перебор (в двух циклах) чисел i и j до %d и вычисление i/j заняло:", limit); 
+		show(&time);
+
+		limit *= 10;
+		cnt--;
 	}
 
-	stop(&time);
-	show(&time);
 	return 0;
 }
